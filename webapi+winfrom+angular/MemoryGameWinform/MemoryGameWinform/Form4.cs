4999126a735652d0c3b2027ddfa860632a985abf
tzviaedelstein1 <tzviaedelstein@seldatinc.com>
2018-10-11 14:20:59 +0300

    com

--- webapi+winfrom+angular/MemoryGameWinform/MemoryGameWinform/Form4.cs
+++ webapi+winfrom+angular/MemoryGameWinform/MemoryGameWinform/Form4.cs
@@ -1,14 +1,10 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 using System.Web.Script.Serialization;
 using System.Windows.Forms;
 
